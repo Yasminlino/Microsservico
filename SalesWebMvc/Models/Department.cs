0746476e5c828de78a541b5b678e8d468e89e716
Yasmin Lino <yasmincriss00@gmail.com>
2024-09-19 11:26:10 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -8,5 +11,26 @@ namespace SalesWebMvc.Models
 
         [StringLength(100)] // Limite o comprimento para 100 caracteres
         public required string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() 
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) 
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(sr => sr.TotalSales(initial, final));
+        }
     }
 }
